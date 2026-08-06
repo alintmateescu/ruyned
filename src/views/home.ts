6c2821adb900567d35383eb2c9b6671f603bbbb2
alintmateescu <alin.t.mateescu@gmail.com>
2026-08-07 02:01:41 +0300

    Update home.ts

--- src/views/home.ts
+++ src/views/home.ts
@@ -94,6 +94,10 @@ ${linkBtn(
   <p>&ldquo;Incredibly believable, pure, genuine and completely authentic. Pull that old denim vest out of the closet and plug in your cassette player!&rdquo;</p>
   <a href="https://www.deadlystormzine.com/2026/07/recenzereview-ruyned-profanum.html" target="_blank" rel="noopener noreferrer" class="press__source">Deadly Storm Zine</a>
 </blockquote>
+<blockquote class="press__item">
+  <p>&ldquo;Gran finale con la furia primordiale di Witches Gall&ouml;w, che vi porter&agrave; a premere subito il pulsante play un&rsquo;altra volta. Adorabili.&rdquo;</p>
+  <a href="https://metalskunk.com/2026/08/06/ruyned-bewitched-hellionight-speedslut-recensione-review/" target="_blank" rel="noopener noreferrer" class="press__source">Metal Skunk (IT)</a>
+</blockquote>
     <blockquote class="press__item">
   <p>&ldquo;... trupa din Timi&#537;oara face parte din categoria restr&acirc;ns&#259; a forma&#539;iilor rom&acirc;ne&#537;ti de thrash metal care pot sta f&#259;r&#259; complexe al&#259;turi de multe dintre numele actuale ale scenei interna&#539;ionale.&rdquo;</p>
   <a href="https://metalfan.ro/2026/07/21/ruyned-profanum-sacrificium/" target="_blank" rel="noopener noreferrer" class="press__source">MetalFan</a>
